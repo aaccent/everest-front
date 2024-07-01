777498f255a89c41ab683dbbf0b470e884d4280c
sevastyan <seva@aaccent.ru>
2024-07-01 12:17:15 +0300

    feat: font utility variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,62 +2,118 @@ import type { Config } from 'tailwindcss'
 import { filters, scrollbarUtilities } from './tailwind/utils'
 import { ICONS } from './tailwind/icons'
 
+export function adaptiveFz(value: number, min = 1, max = 20, mod = 10) {
+  return `clamp(${min}rem, calc(${value} * ${mod} * var(--screen-delta) + ${value}rem), ${max}rem)`
+}
+
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     fontSize: {
-      'header-100': [
-        '9rem',
+      'header-100_desktop': [
+        adaptiveFz(9),
         {
           lineHeight: '100%',
           letterSpacing: '3px',
         },
       ],
-      'header-200': [
-        '6.2rem',
+      'header-100_mobile': [
+        adaptiveFz(5.4),
+        {
+          lineHeight: '100%',
+          letterSpacing: '2px',
+        },
+      ],
+
+      'header-200_desktop': [
+        adaptiveFz(6.2),
         {
           lineHeight: '100%',
           letterSpacing: '3px',
         },
       ],
+      'header-200_mobile': [
+        adaptiveFz(4),
+        {
+          lineHeight: '100%',
+          letterSpacing: '1.5px',
+        },
+      ],
 
-      'header-300': [
-        '5rem',
+      'header-300_desktop': [
+        adaptiveFz(5),
         {
           lineHeight: '100%',
           letterSpacing: '.01em',
         },
       ],
+      'header-300_mobile': [
+        adaptiveFz(3.2),
+        {
+          lineHeight: '90%',
+          letterSpacing: '1px',
+        },
+      ],
 
-      'header-400': [
-        '3.8rem',
+      'header-400_desktop': [
+        adaptiveFz(3.8),
+        {
+          lineHeight: '100%',
+        },
+      ],
+      'header-400_mobile': [
+        adaptiveFz(2.6),
         {
           lineHeight: '100%',
         },
       ],
 
-      'header-500': [
-        '3rem',
+      'header-500_desktop': [
+        adaptiveFz(3),
         {
           lineHeight: '100%',
         },
       ],
-      'base-100-reg-100': [
-        '2.2rem',
+      'header-500_mobile': [
+        adaptiveFz(2.2),
+        {
+          lineHeight: '100%',
+        },
+      ],
+
+      'base-100-reg-100_desktop': [
+        adaptiveFz(2.2),
         {
           lineHeight: '115%',
           letterSpacing: '.01em',
         },
       ],
-      'base-100-lg-100': [
-        '2.2rem',
+      'base-100-reg-100_mobile': [
+        adaptiveFz(1.8),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+        },
+      ],
+
+      'base-100-lg-100_desktop': [
+        adaptiveFz(2.2),
         {
           lineHeight: '115%',
-          letterSpacing: '.01em',
+          letterSpacing: '1px',
           fontWeight: 300,
         },
       ],
-      'base-200-med-100': [
+      'base-100-lg-100_mobile': [
+        adaptiveFz(1.8),
+        {
+          lineHeight: '120%',
+          letterSpacing: '1px',
+          fontWeight: 300,
+        },
+      ],
+
+      'base-200-med-100_desktop': [
         '1.8rem',
         {
           lineHeight: '120%',
@@ -65,67 +121,141 @@ const config: Config = {
           fontWeight: 500,
         },
       ],
-      'base-200-lg-100': [
-        '1.8rem',
+      'base-200-med-100_mobile': [
+        adaptiveFz(1.6),
+        {
+          lineHeight: '130%',
+          letterSpacing: '.01em',
+          fontWeight: 500,
+        },
+      ],
+
+      'base-200-lg-100_desktop': [
+        adaptiveFz(1.8),
         {
           lineHeight: '120%',
           letterSpacing: '.01em',
           fontWeight: 300,
         },
       ],
-      'base-300-reg-100-upper': [
-        '1.6rem',
+      'base-200-lg-100_mobile': [
+        adaptiveFz(1.6),
+        {
+          lineHeight: '130%',
+          letterSpacing: '.01em',
+          fontWeight: 300,
+        },
+      ],
+
+      'base-300-reg-100-upper_desktop': [
+        adaptiveFz(1.6),
         {
           lineHeight: '130%',
           letterSpacing: '.01em',
         },
       ],
-      'base-300-reg-200': [
-        '1.6rem',
-        {
-          lineHeight: '120%',
-          letterSpacing: '.01em',
-        },
-      ],
-      'base-300-lg-100': [
-        '1.6rem',
-        {
-          lineHeight: '120%',
-          letterSpacing: '.01em',
-          fontWeight: 300,
-        },
-      ],
-      'base-400-reg-100': [
-        '1.4rem',
+      'base-300-reg-100-upper_mobile': [
+        adaptiveFz(1.4),
         {
           lineHeight: '130%',
           letterSpacing: '.01em',
         },
       ],
-      'base-400-lg-100': [
-        '1.4rem',
+
+      'base-300-reg-200_desktop': [
+        adaptiveFz(1.6),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+        },
+      ],
+      'base-300-reg-200_mobile': [
+        adaptiveFz(1.4),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+        },
+      ],
+
+      'base-300-lg-100_desktop': [
+        adaptiveFz(1.6),
         {
           lineHeight: '120%',
           letterSpacing: '.01em',
           fontWeight: 300,
         },
       ],
-      'base-500-reg-100-upper': [
-        '1.2rem',
+      'base-300-lg-100_mobile': [
+        adaptiveFz(1.4),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+          fontWeight: 300,
+        },
+      ],
+
+      'base-400-reg-100_desktop': [
+        adaptiveFz(1.4),
+        {
+          lineHeight: '130%',
+          letterSpacing: '.01em',
+        },
+      ],
+      'base-400-reg-100_mobile': [
+        adaptiveFz(1.2),
+        {
+          lineHeight: '130%',
+          letterSpacing: '.01em',
+        },
+      ],
+
+      'base-400-lg-100_desktop': [
+        adaptiveFz(1.4),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+          fontWeight: 300,
+        },
+      ],
+      'base-400-lg-100_mobile': [
+        adaptiveFz(1.2),
+        {
+          lineHeight: '120%',
+          letterSpacing: '.01em',
+          fontWeight: 300,
+        },
+      ],
+
+      'base-500-reg-100-upper_desktop': [
+        adaptiveFz(1.2),
         {
           lineHeight: '140%',
           letterSpacing: '.01em',
         },
       ],
-      'base-500-reg-200': [
-        '1.2rem',
+      'base-500-reg-100-upper_mobile': [
+        adaptiveFz(1),
+        {
+          lineHeight: '140%',
+          letterSpacing: '.01em',
+        },
+      ],
+
+      'base-500-reg-200_desktop': [
+        adaptiveFz(1.2),
+        {
+          lineHeight: '130%',
+          letterSpacing: '.01em',
+        },
+      ],
+      'base-500-reg-200_mobile': [
+        adaptiveFz(1),
         {
           lineHeight: '130%',
           letterSpacing: '.01em',
         },
       ],
     },
-
     colors: {
       base: {
         100: 'hsl(0 0 100)',
@@ -144,7 +274,6 @@ const config: Config = {
         green: 'hsl(137 71 31)',
       },
     },
-
     extend: {
       backgroundImage: {
         'icon-location': ICONS.LOCATION,
