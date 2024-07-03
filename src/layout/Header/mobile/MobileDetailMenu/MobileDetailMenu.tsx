f848fa238eab55ea85ed8713a6b780c937f56fe9
sevastyan <seva@aaccent.ru>
2024-07-03 17:37:37 +0300

    feat: ActionButtons to MobileDetailMenu

--- src/layout/Header/mobile/MobileDetailMenu/MobileDetailMenu.tsx
+++ src/layout/Header/mobile/MobileDetailMenu/MobileDetailMenu.tsx
@@ -5,6 +5,7 @@ import MobileMenuItem from './components/MobileMenuItem'
 import MenuItemCard from '../../components/MenuItemCard'
 import SeeAllCard from '../../components/SeeAllCard'
 import Link from 'next/link'
+import { IconName, ICONS_NAME } from '@/globals/icons/icons'
 
 function showItemsCards(list: any[]) {
   return list.map((item, i) => (
@@ -24,15 +25,37 @@ function showItems(list: any[]) {
   ))
 }
 
+interface ActionButtonProps {
+  text: string
+  href?: string
+  icon: IconName
+  accented?: boolean
+}
+
+function ActionButton({ accented, text, href, icon }: ActionButtonProps) {
+  const inner = (
+    <div
+      className={`p-[16px] h-[115px] flex flex-col justify-between gap-[10px] rounded-[20px] ${accented ? 'bg-primary text-base-100' : 'bg-base-300'}`}
+    >
+      <span className='max-w-[93px] block text-base-300-reg-200 text-left'>{text}</span>
+      <span
+        className={`circle-[40px] flex justify-center items-center bg-base-100 self-end after:size-[21px] ${accented ? 'after:filter-primary' : 'after:filter-base-600'} after:bg-default after:bg-${ICONS_NAME[icon]}`}
+      />
+    </div>
+  )
+
+  return href ? <Link href={href}>{inner}</Link> : <button type='button'>{inner}</button>
+}
+
 async function MobileDetailMenu() {
   const catalog = await getCatalog()
   const services = await getServices()
 
   return (
-    <MobileDetailMenuWrapper className='fixed bottom-0 z-20 w-full h-full bg-base-100 opacity-0 invisible transition-opacity overflow-y-auto peer-[.menu-open]/style-state:visible peer-[.menu-open]/style-state:opacity-100'>
+    <MobileDetailMenuWrapper className='fixed bottom-0 z-20 w-full h-full bg-base-100 opacity-0 invisible transition-opacity peer-[.menu-open]/style-state:visible peer-[.menu-open]/style-state:opacity-100'>
       <div className='absolute inset-x-[20px] top-[24px] h-[1px] bg-base-600/[.1]' />
-      <div className='relative pt-[24px] h-full'>
-        <ul className='px-[20px]'>
+      <div className='relative px-[20px] py-[24px] h-full overflow-y-auto'>
+        <ul className='mb-[32px]'>
           <MobileMenuItem text='Покупка'>
             <SeeAllCard />
             {showItemsCards(catalog)}
@@ -50,6 +73,12 @@ async function MobileDetailMenu() {
           <MobileMenuItem href='' text='О нас' />
           <MobileMenuItem href='#' text='Контакты' />
         </ul>
+        <div className='grid grid-cols-2 gap-[8px]'>
+          <ActionButton text='Заказать звонок' icon='PHONE' accented />
+          <ActionButton text='Добавить объявление' icon='ADD' href='#' />
+          <ActionButton text='г.Абакан' icon='LOCATION' />
+          <ActionButton text='Подобрать квартиру' icon='KEYS' />
+        </div>
       </div>
     </MobileDetailMenuWrapper>
   )
