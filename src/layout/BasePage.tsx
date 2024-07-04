6bd01cfba8fd61c9c1c441363c1304e16df89000
sevastyan <seva@aaccent.ru>
2024-07-04 10:55:23 +0300

    feat: features/adaptive, IsMobile and IsDesktop components

--- src/layout/BasePage.tsx
+++ src/layout/BasePage.tsx
@@ -1,16 +1,20 @@
 import React, { PropsWithChildren } from 'react'
 import Footer from '@/layout/Footer'
 import Header from '@/layout/Header/Header'
+import { AdaptiveProvider } from '@/features/adaptive'
+import { basePageProps } from '@/globals/pageProps'
 
-function BasePage({ children }: PropsWithChildren) {
+async function BasePage({ children }: PropsWithChildren) {
+  const baseProps = await basePageProps()
+
   return (
-    <>
+    <AdaptiveProvider viewport={baseProps.viewport}>
       <Header />
       <div>
         <main>{children}</main>
         <Footer />
       </div>
-    </>
+    </AdaptiveProvider>
   )
 }
 
