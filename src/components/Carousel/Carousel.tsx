04c381ad2370f74b18eb2c7dc18f8b1e2c175a17
Regina <rmyachina@mail.ru>
2024-07-08 15:57:57 +0300

    feature: UsefulLinks

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -5,9 +5,13 @@ import useEmblaCarousel from 'embla-carousel-react'
 import { EmblaCarouselType, EmblaOptionsType } from 'embla-carousel'
 import Link from 'next/link'
 
-type CarouselProps = EmblaOptionsType & PropsWithChildren
+type CarouselProps = EmblaOptionsType &
+  PropsWithChildren & {
+    progressBar?: boolean
+    navigations?: boolean
+  }
 
-function Carousel({ children, ...options }: CarouselProps) {
+function Carousel({ children, progressBar, navigations, ...options }: CarouselProps) {
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev()
@@ -29,35 +33,49 @@ function Carousel({ children, ...options }: CarouselProps) {
     emblaApi.on('reInit', onScroll).on('scroll', onScroll).on('slideFocus', onScroll)
   }, [emblaApi, onScroll])
 
-  return (
-    <div>
-      <div className='md:overflow-hidden' ref={emblaRef}>
-        <div className='flex gap-[12px] md:gap-[16px]'>{children}</div>
-      </div>
-      <div className='absolute right-0 top-[20px] hidden h-[42px] w-[89px] overflow-hidden rounded-[12px] bg-base-300 md:block'>
-        <div className='aftre:block aftre:block relative flex h-full w-full before:absolute before:bottom-0 before:left-[50%] before:h-[8px] before:w-[1px] before:translate-x-1/2 before:bg-base-400 after:absolute after:left-[50%] after:top-0 after:h-[8px] after:w-[1px] after:translate-x-1/2 after:bg-base-400'>
-          <button
-            className='button-prev flex w-1/2 items-center justify-center p-[16px] after:block after:size-full after:rotate-180 after:bg-icon-arrow after:bg-contain after:bg-center after:bg-no-repeat after:filter-base-600'
-            onClick={scrollPrev}
-          ></button>
-          <button
-            className='button-next flex w-1/2 items-center justify-center p-[16px] after:block after:size-full after:bg-icon-arrow after:bg-contain after:bg-center after:bg-no-repeat after:filter-base-600'
-            onClick={scrollNext}
-          ></button>
+  function navigationButtons() {
+    return (
+      <>
+        <div className='absolute right-0 top-[20px] hidden h-[42px] w-[89px] overflow-hidden rounded-[12px] bg-base-300 md:block'>
+          <div className='aftre:block aftre:block relative flex h-full w-full before:absolute before:bottom-0 before:left-[50%] before:h-[8px] before:w-[1px] before:translate-x-1/2 before:bg-base-400 after:absolute after:left-[50%] after:top-0 after:h-[8px] after:w-[1px] after:translate-x-1/2 after:bg-base-400'>
+            <button
+              className='button-prev flex w-1/2 items-center justify-center p-[16px] after:block after:size-full after:rotate-180 after:bg-icon-arrow after:bg-contain after:bg-center after:bg-no-repeat after:filter-base-600'
+              onClick={scrollPrev}
+            ></button>
+            <button
+              className='button-next flex w-1/2 items-center justify-center p-[16px] after:block after:size-full after:bg-icon-arrow after:bg-contain after:bg-center after:bg-no-repeat after:filter-base-600'
+              onClick={scrollNext}
+            ></button>
+          </div>
         </div>
-      </div>
-      <Link
-        href={'#'}
-        className='text-base-500-reg-100-upper absolute right-0 top-0 flex w-[70px] justify-center gap-[4px] overflow-hidden rounded-[12px] bg-base-300 py-[10px] after:block after:size-[14px] after:rotate-90 after:bg-icon-arrow-up after:bg-auto after:bg-center after:bg-no-repeat md:hidden'
-      >
-        все
-      </Link>
-      <div className='relative bottom-[-74px] h-[4px] w-full overflow-hidden rounded-[10px] bg-primary'>
+        <Link
+          href={'#'}
+          className='text-base-500-reg-100-upper absolute right-0 top-0 flex w-[70px] justify-center gap-[4px] overflow-hidden rounded-[12px] bg-base-300 py-[10px] after:block after:size-[14px] after:rotate-90 after:bg-icon-arrow-up after:bg-auto after:bg-center after:bg-no-repeat md:hidden'
+        >
+          все
+        </Link>
+      </>
+    )
+  }
+
+  function progress() {
+    return (
+      <div className='relative bottom-[-74px] hidden h-[4px] w-full overflow-hidden rounded-[10px] bg-primary md:block'>
         <div
           className='absolute bottom-0 left-[-100%] top-0 w-full bg-primary'
           style={{ transform: `translate3d(${scrollProgress}%,0px,0px)` }}
         ></div>
       </div>
+    )
+  }
+
+  return (
+    <div>
+      <div className='md:overflow-hidden' ref={emblaRef}>
+        <div className='flex gap-[12px] md:gap-[16px]'>{children}</div>
+      </div>
+      {navigations && navigationButtons()}
+      {progressBar && progress()}
     </div>
   )
 }
---
+++ src/components/UsefulLinks/UsefulLinks.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Link from 'next/link'
+import Section from '@/layout/Section'
+import { getLinks } from '@/globals/api/methods/getLinks'
+import Image from 'next/image'
+import bavel from '@/assets/static/usefull-links-bavel.svg'
+import Carousel, { CarouselSlide } from '@/components/Carousel/Carousel'
+
+interface Link {
+  id: string
+  url: string
+  name: string
+  objectAmount: string
+}
+
+interface Category {
+  categoryName: string
+  categoryIcon: string
+  linksList: Link[]
+}
+
+async function UsefulLinks() {
+  const data = await getLinks()
+
+  return (
+    <Section containerClassName='usefull'>
+      <h2 className='text-header-200 mb-[24px] uppercase md:mb-[40px]'>Полезные ссылки</h2>
+      <Carousel>{showCategories(data)}</Carousel>
+    </Section>
+  )
+}
+
+function showLinks(links: Link[]) {
+  return links.map((link) => {
+    return (
+      <Link href={link.url} key={link.id} className='flex items-center justify-between'>
+        <div className='text-base-300-reg-200'>{link.name}</div>
+        <div className='text-base-200-med-100 text-primary'>{link.objectAmount}</div>
+      </Link>
+    )
+  })
+}
+
+function showCategories(categories: Category[]) {
+  return categories.map((category, index) => {
+    return (
+      <CarouselSlide
+        className='relative rounded-[24px] bg-base-200 p-[24px] md:max-w-[380px] md:rounded-[32px] md:px-[32px] md:pb-[40px] md:pt-[32px]'
+        key={index}
+      >
+        <Image src={bavel} alt={'#'} className='absolute right-0 top-0' />
+        <div className='mb-[20px] flex items-center gap-[16px] md:mb-[40px]'>
+          <div className='flex size-[60px] items-center justify-center rounded-[16px] bg-base-100 md:size-[80px]'>
+            <Image src={category.categoryIcon} alt={''} width={24} height={24} className='md:size-[42px]' />
+          </div>
+          <div className='text-header-400'>{category.categoryName}</div>
+        </div>
+        <div className='text-base-300-reg-200 flex flex-col gap-[14px] md:gap-[18px]'>
+          {showLinks(category.linksList)}
+        </div>
+      </CarouselSlide>
+    )
+  })
+}
+
+export default UsefulLinks
