e8a822031a79edaa9061e5401695b1c06532787e
foxhable <foxhable@gmail.com>
2024-07-08 16:37:10 +0300

    ci: fix include pattern for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
-    include: ['**/*.{test}.?(c|m)[jt]s?(x)'],
+    include: ['**/*.test.?(c|m)[jt]s?(x)'],
     environment: 'jsdom',
   },
 })
