243fccc77c5d026e0eab5bd6e9e030e89d26b354
foxhable <foxhable@gmail.com>
2024-07-09 13:26:25 +0300

    fix: requiring of prop 'containerClassName' in Section component

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react'
 interface Props extends PropsWithChildren {
   /** @description Классы секции. Для смены заднего фона используй [containerClassName]{@link containerClassName} */
   className?: string
-  containerClassName: string
+  containerClassName?: string
 }
 export default function Section({ children, className, containerClassName }: Props) {
   return (
