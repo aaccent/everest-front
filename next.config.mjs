05ca01657ddfbd6dd4fc086df07d0c13991f6161
foxhable <foxhable@gmail.com>
2024-07-11 13:30:31 +0300

    feat: Img component

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   output: 'standalone',
   staticPageGenerationTimeout: 100,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1680, 1920, 2048],
+    imageSizes: [16, 20, 22, 32, 48, 64, 96, 128, 256, 384],
+  }
 }
 
 export default nextConfig
