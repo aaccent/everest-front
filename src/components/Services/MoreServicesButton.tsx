856ffab9df0da3644d063728d4cf048fd6a1233c
Regina <rmyachina@mail.ru>
2024-07-26 14:00:37 +0300

    fix: eslint errors

--- src/components/Services/MoreServicesButton.tsx
+++ src/components/Services/MoreServicesButton.tsx
@@ -24,7 +24,7 @@ function MoreServicesButton({ hiddenServices }: MoreServicesButtonProps) {
       {full ? showServices(hiddenServices) : null}
       <button
         onClick={onButtonClick}
-        className={`text-base-500-reg-100-upper mt-[16px] flex w-full items-center justify-center rounded-[20px] bg-base-100 py-[18px] text-base-600`}
+        className='text-base-500-reg-100-upper mt-[16px] flex w-full items-center justify-center rounded-[20px] bg-base-100 py-[18px] text-base-600'
       >
         {full ? 'Свернуть' : `показать ещё ${hiddenServices.length}`}
       </button>
