9ea00c68519d8965a2b4db27c030777c0ec968b9
Regina <rmyachina@mail.ru>
2024-07-29 17:18:54 +0300

    fix: change code to seoUrl

--- src/features/link.ts
+++ src/features/link.ts
@@ -3,7 +3,7 @@ import { SubCategory } from '@/types/SubCategory'
 
 export function generateCategoryLink(item: Category | SubCategory, parent?: Category) {
   let link = '/catalog'
-  if (parent) link += `/${parent.code}`
-  link += `/${item.code}`
+  if (parent) link += `/${parent.seoUrl}`
+  link += `/${item.seoUrl}`
   return link
 }
