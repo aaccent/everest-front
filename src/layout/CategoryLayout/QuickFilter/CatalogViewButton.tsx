7324209a83ceb241c5c27a29d2c33047abe28c84
foxhable <foxhable@gmail.com>
2024-07-30 18:14:43 +0300

    feat: CatalogViewButton icons

--- src/layout/CategoryLayout/QuickFilter/CatalogViewButton.tsx
+++ src/layout/CategoryLayout/QuickFilter/CatalogViewButton.tsx
@@ -4,16 +4,18 @@ import React, { useContext } from 'react'
 import { CatalogContext } from '@/layout/CategoryLayout/CatalogContext'
 
 export default function CatalogViewButton() {
-  const { setView } = useContext(CatalogContext)
+  const { setView, view } = useContext(CatalogContext)
 
   return (
     <div className='flex items-center gap-[2px]'>
-      <button className='rounded-[20px] border p-[12px]' onClick={() => setView('tile')}>
-        Плитка
-      </button>
-      <button className='rounded-[20px] border p-[12px]' onClick={() => setView('list')}>
-        Список
-      </button>
+      <button
+        className={`size-[28px] bg-icon-grid-view transition-opacity filter-base-600 bg-default ${view === 'tile' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => setView('tile')}
+      />
+      <button
+        className={`size-[28px] bg-icon-list-view filter-base-600 bg-default ${view === 'list' ? 'opacity-100' : 'opacity-50'}`}
+        onClick={() => setView('list')}
+      />
     </div>
   )
 }
