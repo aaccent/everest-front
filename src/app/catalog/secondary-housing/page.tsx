3b8ad11e8a07d0ca3f4bca6ad07268748a48e1b6
foxhable <foxhable@gmail.com>
2024-07-31 10:44:00 +0300

    refactor: extract viewFunctions() from category page

--- src/app/catalog/secondary-housing/page.tsx
+++ src/app/catalog/secondary-housing/page.tsx
@@ -4,21 +4,17 @@ import { getSecondaryHousing } from '@/globals/api'
 import CatalogContent from '@/layout/catalog/CatalogContent'
 import ObjectCard from '@/components/Cards/ObjectCard/ObjectCard'
 import ObjectFullCard from '@/components/Cards/ObjectCard/ObjectFullCard'
+import { viewFunctions } from '@/features/CatalogContext'
 
 async function Page() {
   const category = await getSecondaryHousing()
 
-  function tileView() {
-    return category.objects.map((item) => <ObjectCard item={item} key={item.id} />)
-  }
-
-  function listView() {
-    return category.objects.map((item) => <ObjectFullCard item={item} key={item.id} />)
-  }
-
   return (
     <CategoryLayout category={category}>
-      <CatalogContent tileClassName='gap-y-[32px] md:gap-y-[56px]' listView={listView()} tileView={tileView()} />
+      <CatalogContent
+        tileClassName='gap-y-[32px] md:gap-y-[56px]'
+        {...viewFunctions(category.objects, ObjectCard, ObjectFullCard)}
+      />
     </CategoryLayout>
   )
 }
--- src/features/CatalogContext.tsx
+++ src/features/CatalogContext.tsx
@@ -21,3 +21,25 @@ export function CatalogProvider({ children }: PropsWithChildren) {
 
   return <CatalogContext.Provider value={{ view, setView }}>{children}</CatalogContext.Provider>
 }
+
+interface ViewComponentProps<TItem extends { id: number }> {
+  item: TItem
+}
+
+type ViewComponent<TItem extends { id: number }> = (props: ViewComponentProps<TItem>) => React.ReactNode
+
+export function viewFunctions<
+  TItem extends {
+    id: number
+  },
+>(list: TItem[], ListViewComponent: ViewComponent<TItem>, TileViewComponent: ViewComponent<TItem>) {
+  function tileView() {
+    return list.map((item) => <TileViewComponent item={item} key={item.id} />)
+  }
+
+  function listView() {
+    return list.map((item) => <ListViewComponent item={item} key={item.id} />)
+  }
+
+  return { listView: listView(), tileView: tileView() }
+}
