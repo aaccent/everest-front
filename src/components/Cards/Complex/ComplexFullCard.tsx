a4115f382a3ee9bf5de2b9502dfa524cbecdb299
sevastyan <seva@aaccent.ru>
2024-08-06 12:13:22 +0300

    refactor: rename formatPrice() to formatPriceShortBy(). Add safe price conversion from string to number. Add doccs

--- src/components/Cards/Complex/ComplexFullCard.tsx
+++ src/components/Cards/Complex/ComplexFullCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Complex, FlatTypes } from '@/types/Complex'
 import Link from 'next/link'
 import { showTags } from '@/components/Cards/Complex/ComplexCard'
-import { formatPrice } from '@/features/price'
+import { formatPriceShortBy } from '@/features/price'
 import { formatStatus } from '@/features/date'
 import Img from '@/ui/Img'
 import { AnyCategory } from '@/types/Category'
@@ -19,7 +19,7 @@ function showObjectTypes(objectTypes: FlatTypes[]) {
         <div className='text-base-650'>
           от {object.minArea} м<sup>2</sup>
         </div>
-        <div className=''>{formatPrice(Number(object.minPrice))}</div>
+        <div className=''>{formatPriceShortBy(Number(object.minPrice))}</div>
       </div>
     )
   })
@@ -53,8 +53,8 @@ function ComplexFullCard({ item, category }: Props) {
         <div className='text-base-200-lg-100 mb-[17px] flex items-center gap-[10px] opacity-50 before:block before:h-[15px] before:w-[12px] before:bg-icon-address before:bg-default-auto'>
           {item.address ? item.address : 'нет адреса'}
         </div>
-        <span className='text-header-400 mr-[12px]'>{formatPrice(item.minPrice)}</span>
-        <span className='text-header-400 line-through opacity-50'>{formatPrice(item.priceDiscount)}</span>
+        <span className='text-header-400 mr-[12px]'>{formatPriceShortBy(item.minPrice)}</span>
+        <span className='text-header-400 line-through opacity-50'>{formatPriceShortBy(item.priceDiscount)}</span>
         <div className='mt-[40px] flex flex-col gap-[9px]'>{showObjectTypes(item.objectsType)}</div>
       </div>
       <div className='ml-auto flex flex-col justify-between'>
--- src/features/price.ts
+++ src/features/price.ts
@@ -1,10 +1,34 @@
-export function formatPrice(price: number | null | undefined) {
-  if (!price) return 'нет цены'
-  let shortPrice = price
-  const digits = price.toString().length
+const PRICE_PLACEHOLDER = 'нет цены'
+
+/**
+ * Форматирование `price` для вывода вида `от 5.5 млн ₽`
+ * @param price Любой примитивный тип.
+ * Переданный тип преобразуется в нужный для работы функции.
+ * Безопасно работает с пустыми и неопределенными значениями
+ * @return В зависимости от типа `price`:
+ * * `undefined` или `null` - возвращает [заглушку]{@link PRICE_PLACEHOLDER}.
+ * * `string` - конвертирует через [parseInt()]{@link parseInt},
+ * если результат конвертации `NaN` - возвращает [заглушку]{@link PRICE_PLACEHOLDER},
+ * иначе форматированную строку.
+ * * `number` - возвращает форматированную строку.
+ */
+export function formatPriceShortBy(price: number | string | null | undefined) {
+  if (!price) return PRICE_PLACEHOLDER
+
+  const _price = typeof price === 'string' ? parseInt(price) : price
+  if (Number.isNaN(_price)) return PRICE_PLACEHOLDER
+
+  let shortPrice = _price
+  const digits = _price.toString().length
 
   if (digits > 6) {
-    shortPrice = price / 1000000
+    shortPrice = _price / 1000000
   }
+
   return `от ${shortPrice} млн ₽`
 }
+
+/**
+ * Форматирование `price` для вывода вида `5 000 000 ₽`
+ */
+export function formatFullPrice() {}
