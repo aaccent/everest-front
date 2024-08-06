dc92cdcb4096444874fe8e304769bf80d1db7041
Regina <rmyachina@mail.ru>
2024-08-06 10:56:00 +0300

    fix: Complex Card interaction

--- src/components/Cards/Complex/ComplexCard.tsx
+++ src/components/Cards/Complex/ComplexCard.tsx
@@ -22,22 +22,22 @@ interface Props {
 }
 
 function ComplexCard({ item: props }: Props) {
+  const link = generateCategoryLink(props, { code: 'new-building/complexes' })
   return (
-    <Link
-      href={generateCategoryLink(props, { code: 'new-building/complexes' })}
-      className='group relative block h-[250px] w-full overflow-hidden rounded-[20px] md:h-[388px] md:rounded-[24px]'
-    >
-      <Image
-        className='object-cover object-center transition-transform duration-500 hover:scale-110 hover:transition-transform hover:duration-500'
-        src='/no-photo.jpg'
-        alt=''
-        fill
-      />
+    <div className='relative block h-[250px] w-full overflow-hidden rounded-[20px] md:h-[388px] md:rounded-[24px]'>
+      <Link href={link}>
+        <Image
+          className='object-cover object-center transition-transform duration-500 hover:scale-110 hover:transition-transform hover:duration-500'
+          src='/no-photo.jpg'
+          alt=''
+          fill
+        />
+      </Link>
       <div className='absolute left-[8px] top-[10px] flex gap-[4px] md:left-[14px] md:top-[14px]'>
         {props.tags && showTags(props.tags)}
       </div>
-      <CardInfo {...props} />
-    </Link>
+      <CardInfo complex={{ ...props }} link={link} />
+    </div>
   )
 }
 
