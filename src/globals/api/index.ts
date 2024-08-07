ef6fb622bb4ae55644ff1b5ebbccacfde780fa46
foxhable <foxhable@gmail.com>
2024-08-07 09:22:44 +0300

    refactor: change folder structure for some api methods

--- src/globals/api/index.ts
+++ src/globals/api/index.ts
@@ -4,7 +4,7 @@ export * from './methods/catalog-details'
 
 export * from './methods/getAddresses'
 export * from './methods/getLinks'
-export * from './methods/getNewBuildingsOnMain'
+export * from './methods/main-page/getNewBuildingsOnMain'
 export * from './methods/getObjects'
 export * from './methods/getServices'
 export * from './methods/getSocials'
