3899a5a1508c1d218b2d17c01bb3e0cdde6eb21f
Regina <rmyachina@mail.ru>
2024-08-13 15:09:02 +0300

    feat: input type range

--- src/page-components/gk-detailed/LayoutChoice/LayoutChoice.tsx
+++ src/page-components/gk-detailed/LayoutChoice/LayoutChoice.tsx
@@ -7,6 +7,7 @@ import { ComplexDetailedHouse } from '@/types/Complex'
 import LayoutInner from '@/page-components/gk-detailed/LayoutChoice/LayoutInner'
 import LayoutList from '@/page-components/gk-detailed/LayoutChoice/LayoutList'
 import LayoutTypes from '@/page-components/gk-detailed/LayoutChoice/LayoutTypes'
+import Range from '@/ui/inputs/Range'
 
 interface LayoutChoiceProps {
   houses: ComplexDetailedHouse[]
@@ -18,6 +19,8 @@ function LayoutChoice(props: LayoutChoiceProps) {
       <h2 className='text-header-200 mb-[32px] font-coolvetica uppercase'>Выбор планировки</h2>
       <Container className='mb-[40px] hidden justify-between rounded-[32px] bg-base-200 md:flex md:px-[32px] md:py-[32px]'>
         <SelectorInline list={['Студия', 1, 2, 3, 4]} className='rounded-[16px] bg-base-100' />
+        <Range min={1} max={100} units='млн.₽' />
+        <Range min={40} max={86} units='м2' />
         <Button variation='primary' size='small' text='Показать 442 объекта' />
       </Container>
       <LayoutInner listView={<LayoutList houses={props.houses} />} typeView={<LayoutTypes />} />
---
+++ src/ui/inputs/Range.tsx
@@ -0,0 +1,99 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+
+interface RangeProps {
+  min: number
+  max: number
+  units?: string
+}
+
+function Range({ min, max, units }: RangeProps) {
+  const [minVal, setMinVal] = useState(min)
+  const [maxVal, setMaxVal] = useState(max)
+  const [value, setValue] = useState({ min: minVal, max: maxVal })
+  const step = 0.1
+
+  useEffect(() => {
+    setMinVal(value.min)
+    setMaxVal(value.max)
+  }, [value])
+
+  const onMinValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+
+    if (+e.target.value >= maxVal) return
+    const newMinVal = Math.min(+e.target.value, maxVal - step)
+    setMinVal(newMinVal)
+    setValue({ min: newMinVal, max: maxVal })
+  }
+  const onMaxValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    if (+e.target.value <= minVal) return
+    const newMaxVal = Math.max(+e.target.value, minVal + step)
+    setMaxVal(newMaxVal)
+    setValue({ min: minVal, max: newMaxVal })
+  }
+
+  const minPos = ((minVal - min) / (max - min)) * 100
+  const maxPos = ((maxVal - min) / (max - min)) * 100
+  return (
+    <div className='text-base-400-lg-100 relative min-w-[273px] rounded-[16px] bg-base-100 px-[15px] py-[12px]'>
+      <div className='relative flex items-center justify-between after:absolute after:inset-1/2 after:block after:h-[12px] after:w-[1px] after:-translate-x-1/2 after:-translate-y-1/2 after:bg-base-400'>
+        <div className='text-base-650'>
+          от
+          <span className='text-base-600'>
+            {' '}
+            {minVal} {units}
+          </span>
+        </div>
+        <div className='text-base-650'>
+          от
+          <span className='text-base-600'>
+            {' '}
+            {maxVal} {units}
+          </span>
+        </div>
+      </div>
+      <div className='absolute inset-x-0 bottom-0 h-[12px]'>
+        <div className='absolute inset-x-[12px] inset-y-0'>
+          <input
+            type='range'
+            step={step}
+            min={min}
+            max={max}
+            onChange={onMinValChange}
+            className='track-transparent'
+            value={minVal}
+          />
+          <input
+            type='range'
+            step={step}
+            min={min}
+            max={max}
+            onChange={onMaxValChange}
+            className='track-transparent'
+            value={maxVal}
+          />
+        </div>
+        <div className='absolute bottom-0 left-[12px] right-[24px] h-[2px]'>
+          <div
+            style={{ left: `${minPos}%` }}
+            className='absolute top-1/2 size-[12px] -translate-y-1/2 rounded-full bg-primary'
+          ></div>
+          <div className='absolute inset-x-0 inset-y-0 bg-transparent'>
+            <div
+              style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
+              className='absolute inset-0 bg-primary'
+            ></div>
+          </div>
+          <div
+            style={{ left: `${maxPos}%` }}
+            className='absolute right-0 top-1/2 size-[12px] -translate-y-1/2 rounded-full bg-primary'
+          ></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Range
