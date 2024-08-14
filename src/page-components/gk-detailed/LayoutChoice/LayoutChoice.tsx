eca90353cb557b08c84907414f77c48ea2e6e9ec
Regina <rmyachina@mail.ru>
2024-08-14 16:29:10 +0300

    refactor: Range func

--- src/page-components/gk-detailed/LayoutChoice/LayoutChoice.tsx
+++ src/page-components/gk-detailed/LayoutChoice/LayoutChoice.tsx
@@ -19,7 +19,7 @@ function LayoutChoice(props: LayoutChoiceProps) {
       <h2 className='text-header-200 mb-[32px] font-coolvetica uppercase'>Выбор планировки</h2>
       <Container className='mb-[40px] hidden justify-between rounded-[32px] bg-base-200 md:flex md:px-[32px] md:py-[32px]'>
         <SelectorInline list={['Студия', 1, 2, 3, 4]} className='rounded-[16px] bg-base-100' />
-        <Range min={1} max={100} units='млн.₽' />
+        <Range min={5} max={100} units='млн.₽' />
         <Range min={40} max={86} units='м2' />
         <Button variation='primary' size='small' text='Показать 442 объекта' />
       </Container>
--- src/ui/inputs/Range.tsx
+++ src/ui/inputs/Range.tsx
@@ -1,83 +1,6 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { IMaskInput } from 'react-imask'
-
-interface InputInRangeProps {
-  min: number
-  max: number
-  onMaxChangeValue: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onMinChangeValue: (e: React.ChangeEvent<HTMLInputElement>) => void
-  units?: string
-}
-
-function InputsInRange({ min, max, onMinChangeValue, onMaxChangeValue, units }: InputInRangeProps) {
-  const [inputValue, setInputValue] = useState({ min, max })
-
-  useEffect(() => {
-    setInputValue({
-      min,
-      max,
-    })
-  }, [min, max])
-
-  const onMinInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue({
-      min: +e.target.value,
-      max: inputValue.max,
-    })
-    onMinChangeValue(e)
-  }
-  const onMaxInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue({
-      min: inputValue.min,
-      max: +e.target.value,
-    })
-    onMaxChangeValue(e)
-  }
-
-  return (
-    <div className='relative flex items-center justify-between after:absolute after:inset-1/2 after:block after:h-[12px] after:w-[1px] after:-translate-x-1/2 after:-translate-y-1/2 after:bg-base-400'>
-      <label className='w-[86px] text-base-650' id='min'>
-        <span className='text-base-650'>от </span>
-        <IMaskInput
-          mask={`0[00000000000000000000000000] [${units}]`}
-          className='w-full text-base-600 focus:outline-0'
-          value={`${inputValue.min ? inputValue.min : ''} ${units}`}
-          name='min'
-          onFocus={() =>
-            setInputValue({
-              min: 0,
-              max: inputValue.max,
-            })
-          }
-          onBlur={(e: React.ChangeEvent<HTMLInputElement>) => {
-            if (!e.target.value) return setInputValue({ min: min, max: inputValue.max })
-          }}
-          onInput={onMinInput}
-        />
-      </label>
-      <label className='w-[86px] text-base-650' id='min'>
-        <span className='text-base-650'>до </span>
-        <IMaskInput
-          mask={`0[00000000000000000000000000] [${units}]`}
-          className='w-full text-base-600 focus:outline-0'
-          value={`${inputValue.max ? inputValue.max : ''} ${units}`}
-          name='max'
-          onFocus={() =>
-            setInputValue({
-              min: inputValue.min,
-              max: 0,
-            })
-          }
-          onBlur={(e: React.ChangeEvent<HTMLInputElement>) => {
-            if (!e.target.value) return setInputValue({ min: inputValue.min, max: max })
-          }}
-          onInput={onMaxInput}
-        />
-      </label>
-    </div>
-  )
-}
 
 interface RangeProps {
   min: number
@@ -90,44 +13,66 @@ function Range({ min, max, units }: RangeProps) {
   const step = 0.1
 
   const onMinValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (+e.target.value >= value.max) return
-    const newMinVal = Math.min(+e.target.value, value.max - step)
+    if (+e.target.value > value.max - 5) return
     setValue({
-      min: newMinVal,
+      min: +e.target.value,
       max: value.max,
     })
   }
   const onMaxValChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (+e.target.value <= value.min) return
-    const newMaxVal = Math.max(+e.target.value, value.min + step)
+    if (+e.target.value < value.min + 5) return
     setValue({
       min: value.min,
-      max: newMaxVal,
+      max: +e.target.value,
     })
+  }
+
+  const onMinInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (+e.target.value >= min && +e.target.value < value.max) {
+      setValue({ min: +e.target.value, max: value.max })
+    }
+  }
+  const onMaxInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (+e.target.value > value.min && +e.target.value <= max) {
+      setValue({ min: value.min, max: +e.target.value })
+    }
   }
 
   const minPos = ((value.min - min) / (max - min)) * 100
   const maxPos = ((value.max - min) / (max - min)) * 100
+
   return (
     <div className='text-base-400-lg-100 relative min-w-[273px] rounded-[16px] bg-base-100 px-[15px] py-[12px]'>
-      <InputsInRange
-        min={value.min}
-        max={value.max}
-        onMaxChangeValue={onMaxValChange}
-        onMinChangeValue={onMinValChange}
-        units={units}
-      />
+      <div className='text-base-400-lg-100 flex items-center justify-center'>
+        <label>
+          <span className='text-base-650'>от</span>
+          <IMaskInput
+            size={3}
+            mask='0[00][.][0]'
+            min={min}
+            max={value.max}
+            className='px-[5px] focus:outline-0'
+            value={value.min.toString()}
+            onChange={onMinInputChange}
+          />
+          <span>{units}</span>
+        </label>
+        <div className='mx-[78px] h-[12px] w-[1px] bg-base-400 md:mx-[56px]' />
+        <label>
+          <span className='text-base-650'>до</span>
+          <IMaskInput
+            size={3}
+            mask='0[00][.][0]'
+            className='px-[5px] focus:outline-0'
+            value={value.max.toString()}
+            onChange={onMaxInputChange}
+          />
+          <span>{units}</span>
+        </label>
+      </div>
       <div className='absolute inset-x-0 bottom-0 h-[12px]'>
         <div className='absolute inset-x-[12px] inset-y-0'>
-          <input
-            type='range'
-            step={step}
-            min={min}
-            max={max}
-            onChange={onMinValChange}
-            className='track-transparent'
-            value={value.min}
-          />
+          <input type='range' step={step} onChange={onMinValChange} className='track-transparent' value={value.min} />
           <input
             type='range'
             step={step}
