793ffc2cb804c094459d996009166206ab68e9f1
Regina <rmyachina@mail.ru>
2024-09-10 14:53:28 +0300

    refactor: replace FilterTags

--- src/components/Popup/FilterPopup/FilterPopup.tsx
+++ src/components/Popup/FilterPopup/FilterPopup.tsx
@@ -4,69 +4,25 @@ import MapObjectsButton from '@/ui/buttons/MapObjectsButton'
 import ClosePopupButton from '@/ui/buttons/ClosePopupButton'
 import Button from '@/ui/buttons/Button'
 import { getFilters } from '@/globals/api/methods/getFilters'
-import { FilterBlock, FilterType, FilterView } from '@/types/FiltersType'
+import { FilterBlock } from '@/types/FiltersType'
 import { IsDesktop, IsMobile } from '@/features/adaptive'
 import MobileFilterItem from '@/components/Popup/FilterPopup/MobileFilterItem'
 import { FilterItems } from '@/features/FilterItems'
-import { useCategoryFilter } from '@/features/useCategoryFilter'
-
-function formatTagText(f: FilterType<FilterView>) {
-  return f.fieldType === 'range'
-    ? `${f.name} : ${f.value.min} - ${f.value.max}`
-    : f.fieldType === 'toggle'
-      ? `Есть ${f.name}`
-      : `${f.name} : ${f.value}`
-}
-
-function FiltersTags({ className, list }: FiltersTagsProps) {
-  function showTags() {
-    return list.map((f) => (
-      <button
-        className='text-base-400-lg-100 flex items-center gap-[4px] rounded-[50px] bg-primary py-[6.5px] pl-[12px] pr-[7px] text-base-100 after:block after:size-[16px] after:bg-icon-close after:bg-default-contain'
-        key={f.id}
-      >
-        {formatTagText(f)}
-      </button>
-    ))
-  }
-
-  return <div className={className}>{showTags()}</div>
-}
+import ResetFiltersButton from '@/components/QuickFilter/ResetFiltersButton'
+import { FiltersTags } from '@/components/QuickFilter/FilterTags'
 
 interface Props {
   category: string
 }
 
 function FilterPopup({ category }: Props) {
-  const [filters, setFilters] = useState<FilterBlock[]>()
-  const { filter } = useCategoryFilter()
-  const [activeFilters, setActiveFilters] = useState<FilterType<FilterView>[]>([])
-
-  function getActiveFilters(filtersGeneral: FilterBlock[]) {
-    const allFiltersList = filtersGeneral.map((block) => block.filters).reduce((acc, f) => acc.concat(f), [])
-
-    const activeId = filter.parsed.map((filter) => filter.id)
-    const activeFilters = allFiltersList.filter((filter) => activeId.includes(filter.id))
-    return activeFilters.map((f) => {
-      return {
-        id: f.id,
-        name: f.name,
-        value: filter.parsed.find((item) => item.id === f.id)!.value,
-        fieldType: f.fieldType,
-      }
-    })
-  }
+  const [filters, setFilters] = useState<FilterBlock[]>([])
 
   useEffect(() => {
     getFilters(category).then((res) => {
       setFilters(res.filters)
-      setActiveFilters(getActiveFilters(res.filters) as FilterType<FilterView>[])
     })
   }, [])
-
-  useEffect(() => {
-    //setActiveFilters()
-  }, [filter.str])
 
   function showFiltersBlocks() {
     return filters?.map((block) => (
@@ -95,22 +51,23 @@ function FilterPopup({ category }: Props) {
         <div className='*:mb-[18px] md:w-full md:max-w-[1140px] md:*:mb-[60px]'>{showFiltersBlocks()}</div>
       </div>
 
-      <div className='fixed bottom-0 left-0 z-10 flex w-full justify-between bg-base-100 px-[24px] py-[16px] md:justify-normal md:border-t md:border-t-base-400 md:px-[56px] md:py-[24px]'>
+      <div className='fixed bottom-0 left-0 z-10 flex w-full items-center justify-between bg-base-100 px-[24px] py-[16px] md:justify-normal md:border-t md:border-t-base-400 md:px-[56px] md:py-[24px]'>
         <button className='flex size-[50px] items-center justify-center rounded-[16px] bg-base-300 after:block after:size-[22px] after:bg-icon-search-favorite after:bg-default-contain md:hidden' />
-        <Button variation='second' size='small' text='Сбросить' className='md:order-2' />
-        <Button variation='primary' size='small' text='Показать 27 объектов' className='md:order-1 md:mr-[12px]' />
 
-        <FiltersTags className='order-3 ml-[176px] hidden items-center gap-[10px] md:flex' list={activeFilters} />
+        <Button variation='primary' size='small' text='Показать 27 объектов' className='md:mr-[12px]' />
+        <ResetFiltersButton
+          text='Сбросить фильтры'
+          className='rounded-[16px] bg-base-300 px-[28px] py-[12px] transition-colors hover:bg-primary hover:text-base-100'
+        />
+        <FiltersTags
+          className='ml-[176px] mr-[20px] hidden overflow-auto scrollbar-transparent md:flex'
+          list={filters}
+        />
 
         <MapObjectsButton className='ml-auto hidden md:order-3 md:flex' />
       </div>
     </>
   )
-}
-
-interface FiltersTagsProps {
-  className?: string
-  list: FilterType<FilterView>[]
 }
 
 export default FilterPopup
---
+++ src/components/QuickFilter/FilterTags.tsx
@@ -0,0 +1,83 @@
+'use client'
+import { FilterBlock, FilterType, FilterView } from '@/types/FiltersType'
+import { useCategoryFilter } from '@/features/useCategoryFilter'
+import React, { useEffect, useState } from 'react'
+import { getFilters } from '@/globals/api/methods/getFilters'
+
+interface Type {
+  id: number
+  name: string
+  value: any
+  fieldType: FilterView['fieldType']
+}
+
+function formatTagText(f: Type) {
+  switch (f.fieldType) {
+    case 'range':
+      return `${f.name}: ${f.value[0]} - ${f.value[1]}`
+    case 'toggle':
+      return `Есть ${f.name}`
+    default:
+      return `${f.name}: ${f.value}`
+  }
+}
+
+type FiltersTagsProps = {
+  className?: string
+} & (
+  | {
+      category: string
+      list?: never
+    }
+  | {
+      list: FilterBlock[]
+      category?: never
+    }
+)
+
+export function FiltersTags({ className, category, list }: FiltersTagsProps) {
+  const [activeFilters, setActiveFilters] = useState<FilterType<FilterView>[]>([])
+  const { removeFilter, filter } = useCategoryFilter()
+
+  function getActiveFilters(filtersGeneral: FilterBlock[]) {
+    if (!filtersGeneral.length) return []
+    const allFiltersList = filtersGeneral.map((block) => block.filters).reduce((acc, f) => acc.concat(f), [])
+    const activeId = filter.parsed.map((filter) => filter.id)
+    const activeFilters = allFiltersList.filter((filter) => activeId.includes(filter.id))
+    return activeFilters.map((f) => {
+      return {
+        id: f.id,
+        name: f.name,
+        value: filter.parsed.find((item) => item.id === f.id)!.value,
+        fieldType: f.fieldType,
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (category) {
+      getFilters(category).then((res) => {
+        setActiveFilters(getActiveFilters(res.filters) as FilterType<FilterView>[])
+      })
+    }
+    if (list) {
+      setActiveFilters(getActiveFilters(list) as FilterType<FilterView>[])
+    }
+  }, [list, filter])
+
+  function showTags() {
+    return activeFilters.map((f) => {
+      return f.value ? (
+        <button
+          className='text-base-400-lg-100 flex items-center gap-[4px] text-nowrap rounded-[50px] bg-primary py-[6.5px] pl-[12px] pr-[7px] text-base-100 after:block after:size-[16px] after:bg-icon-close after:filter-base-100 after:bg-default-auto'
+          key={f.id}
+          onClick={() => removeFilter(f.id)}
+        >
+          {formatTagText(f)}
+        </button>
+      ) : null
+    })
+  }
+
+  return <div className={`flex items-center gap-[10px] ${className}`}>{showTags()}</div>
+}
