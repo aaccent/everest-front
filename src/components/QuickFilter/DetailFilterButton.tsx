6de3d426c5c7e2086e5c40a8b4ab80e4dc33065a
Regina <rmyachina@mail.ru>
2024-09-11 09:24:16 +0300

    feat: DetailFilterButton

--- src/components/QuickFilter/DetailFilterButton.tsx
+++ src/components/QuickFilter/DetailFilterButton.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { PopupContext } from '@/components/Popup/Popup'
+import { useCategoryFilter } from '@/features/useCategoryFilter'
 
 interface Props {
   className?: string
@@ -10,12 +11,24 @@ interface Props {
 
 function DetailFilterButton({ className, category }: Props) {
   const { openPopup } = useContext(PopupContext)
+  const { filter } = useCategoryFilter()
+  const [count, setCount] = useState<number>(filter.parsed.length)
+  useEffect(() => {
+    setCount(filter.parsed.length)
+  }, [filter])
   return (
-    <button
-      className={`${className} flex size-[42px] items-center justify-center rounded-[12px] bg-base-100 p-[10px] after:size-[21px] after:bg-icon-filter after:bg-default`}
-      type='button'
-      onClick={() => openPopup({ name: 'filterPopup', args: { category } })}
-    />
+    <div className='relative'>
+      <div
+        className={`text-base-500-reg-200 absolute right-[5px] top-[-5px] hidden items-center justify-center bg-primary text-base-100 circle-[24px] md:flex ${filter.parsed.length ? '' : 'md:hidden'}`}
+      >
+        {count}
+      </div>
+      <button
+        className={`${className} flex size-[42px] items-center justify-center rounded-[12px] bg-base-100 p-[10px] after:size-[21px] after:bg-icon-filter after:bg-default`}
+        type='button'
+        onClick={() => openPopup({ name: 'filterPopup', args: { category } })}
+      />
+    </div>
   )
 }
 
