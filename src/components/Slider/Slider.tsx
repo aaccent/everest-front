aa7e36e85ba010966891f042d66f34645af531c3
Regina <rmyachina@mail.ru>
2024-09-12 16:33:24 +0300

    fix: sliders on main page

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -29,14 +29,19 @@ function Slider({ list, type, link }: SliderProps) {
   function showSlides() {
     switch (type) {
       case 'complex':
-        return list.map((card) => (
-          <CarouselSlide
-            className='mr-[12px] max-w-[320px] md:mr-0 md:max-w-none md:basis-1/3 md:pr-[16px] md:[&:nth-child(3n)]:pr-0 md:[&:nth-child(4n)]:pl-[16px]'
-            key={card.id}
-          >
-            <ComplexCard item={card} />
-          </CarouselSlide>
-        ))
+        return (
+          <>
+            {list.map((card) => (
+              <CarouselSlide
+                className='mr-[12px] max-w-[320px] md:mr-0 md:max-w-none md:basis-1/3 md:pr-[16px] md:[&:nth-child(3n)]:pr-0 md:[&:nth-child(4n)]:pl-[16px]'
+                key={card.id}
+              >
+                <ComplexCard item={card} />
+              </CarouselSlide>
+            ))}
+            <SeeAllCard link={link} />
+          </>
+        )
       case 'objects':
         return list.map((card) => (
           <CarouselSlide
@@ -52,12 +57,9 @@ function Slider({ list, type, link }: SliderProps) {
   return (
     <>
       <Carousel>
-        <CarouselInner>
-          {showSlides()}
-          <SeeAllCard link={link} />
-        </CarouselInner>
+        <CarouselInner>{showSlides()}</CarouselInner>
         <CarouselNavigations />
-        <CarouselProgressBar className='hidden md:block' perView={3} />
+        <CarouselProgressBar className='hidden md:mt-[70px] md:block' perView={3} />
       </Carousel>
     </>
   )
