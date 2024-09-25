6cf32d75dfc145e86cc3ac863b4a06bcff6e4c70
Regina <rmyachina@mail.ru>
2024-09-25 17:34:18 +0300

    fix:bugs

--- src/components/Slider/SliderSection.tsx
+++ src/components/Slider/SliderSection.tsx
@@ -24,7 +24,7 @@ async function SliderSection(props: Props) {
       <div className='md: mb-[32px] flex items-center justify-between'>
         <h2 className='text-header-200 font-coolvetica uppercase'>{props.title}</h2>
         <Link
-          href='#'
+          href={props.type === 'complex' ? props.link : '/catalog/secondary-housing'}
           className='text-base-500-reg-100-upper flex w-[70px] justify-center gap-[4px] overflow-hidden rounded-[12px] bg-base-300 py-[10px] after:block after:size-[14px] after:rotate-90 after:bg-icon-arrow-up after:filter-base-600 after:bg-default-contain md:hidden'
         >
           все
--- src/ui/popups/FilterPopup/FilterPopup.tsx
+++ src/ui/popups/FilterPopup/FilterPopup.tsx
@@ -44,7 +44,7 @@ function FilterPopup({ category }: Props) {
   return (
     <DynamicPopup popupName='filterPopup'>
       <div className='absolute inset-x-0 bottom-0 flex h-[calc(100dvh-64px)] flex-col rounded-[24px] bg-base-100 scrollbar-custom md:top-[48px] md:block md:h-full md:overflow-auto md:p-[56px]'>
-        <div className='relative h-1 grow p-[24px] md:static md:h-fit md:grow-0 md:pb-[100px]'>
+        <div className='relative h-1 grow p-[24px] md:static md:h-fit md:grow-0 md:pb-[250px]'>
           <div className='mb-[33px] flex items-center justify-between md:mb-[56px]'>
             <MapObjectsButton className='md:hidden' />
             <div className='text-header-300 md:text-header-200 md:uppercase'>Фильтры</div>
