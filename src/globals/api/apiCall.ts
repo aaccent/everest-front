821ffd7db58004eb203f17066c014409f64e12ee
Regina <rmyachina@mail.ru>
2024-10-01 17:02:24 +0300

    remove comment

--- src/globals/api/apiCall.ts
+++ src/globals/api/apiCall.ts
@@ -101,7 +101,6 @@ export async function apiCall<TRequest extends APIRequest | false = false, TResp
     const searchParams = convertObjectToURLSearchParams(request)
     url += `?${searchParams.toString()}`
   }
-  //if(url.includes('catalog')) console.log(url)
 
   const res = await fetch(url, fetchInit)
   const text = await res.text()
