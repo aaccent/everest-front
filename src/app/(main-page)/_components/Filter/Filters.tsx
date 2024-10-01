34a59ffde763d4b0bcbda17aa6b92634cea17d8a
Regina <rmyachina@mail.ru>
2024-10-01 16:22:09 +0300

    feat: TabButtons

--- src/app/(main-page)/_components/Filter/Filters.tsx
+++ src/app/(main-page)/_components/Filter/Filters.tsx
@@ -4,6 +4,7 @@ import { getCatalogMenu } from '@/globals/api'
 import CategoryButton, { CategoryButtonProps } from '@/app/(main-page)/_components/Filter/CategoryButton'
 import { MenuCategory } from '@/types/Menu'
 import CategoryFilter from '@/app/(main-page)/_components/Filter/CategoryFilter'
+import TabButtons from '@/components/TabButtons'
 
 function Filters() {
   const [categories, setCategories] = useState<MenuCategory[]>([])
@@ -38,36 +39,27 @@ function Filters() {
     ))
   }
 
+  function onTabButtonClick(value: string) {
+    const isRent = value === 'rent'
+    return setActiveCategory({
+      seoUrl: activeCategory.seoUrl,
+      isRent,
+    })
+  }
+
   return (
     <div className='absolute inset-x-0 bottom-[20px] overflow-auto scrollbar-transparent md:inset-x-[56px] md:bottom-[56px]'>
       <div className='flex w-fit gap-[8px] overflow-hidden rounded-t-[24px] md:gap-0 md:bg-base-650'>
         {showCategoryButtons()}
       </div>
       <div className='hidden rounded-b-[24px] rounded-tr-[24px] bg-base-100 p-[20px] md:block'>
-        <div className='items-center gap-[8px]'>
-          <button
-            onClick={() =>
-              setActiveCategory({
-                seoUrl: activeCategory.seoUrl,
-                isRent: false,
-              })
-            }
-            className={`text-base-500-reg-100-upper rounded-[50px] px-[14px] py-[9px] ${!activeCategory.isRent ? 'bg-primary text-base-100' : 'bg-base-300 text-base-600'}`}
-          >
-            Купить
-          </button>
-          <button
-            onClick={() =>
-              setActiveCategory({
-                seoUrl: activeCategory.seoUrl,
-                isRent: true,
-              })
-            }
-            className={`text-base-500-reg-100-upper rounded-[50px] px-[14px] py-[9px] ${activeCategory.isRent ? 'bg-primary text-base-100' : 'bg-base-300 text-base-600'}`}
-          >
-            Снять
-          </button>
-        </div>
+        <TabButtons
+          list={[
+            { text: 'Купить', value: 'buy' },
+            { text: 'Снять', value: 'rent' },
+          ]}
+          onChange={onTabButtonClick}
+        />
         <CategoryFilter categoryName={activeCategory.seoUrl} rent={activeCategory.isRent} />
       </div>
     </div>
