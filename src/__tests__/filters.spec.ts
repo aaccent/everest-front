1ed308b0ab2c8fe46ebb2eb97cfc85ca57b0c0aa
Regina <rmyachina@mail.ru>
2024-10-02 11:45:36 +0300

    test: filter

--- src/__tests__/filters.spec.ts
+++ src/__tests__/filters.spec.ts
@@ -5,11 +5,13 @@ test.describe('Filters', () => {
   test.beforeEach('Load page', async ({ page }) => {
     await page.goto(ROUTES.SECONDARY_HOUSING)
   })
+
   test('Should add get parameter to url', { tag: '@only-desktop' }, async ({ page }) => {
     const filterOption = page.locator('[data-filter="selector-inline-option"]').first()
     await filterOption.click()
     expect(page.url().includes('?filter')).toBeTruthy()
   })
+
   test('Filter tags should appear', { tag: '@only-desktop' }, async ({ page }) => {
     const filterOption = page.locator('[data-filter="selector-inline-option"]').first()
     await filterOption.click()
@@ -18,6 +20,7 @@ test.describe('Filters', () => {
       timeout: 10000,
     })
   })
+
   test('Clear filters', { tag: '@only-desktop' }, async ({ page }) => {
     const filterOption = page.locator('[data-filter="selector-inline-option"]').first()
     await filterOption.click()
@@ -29,11 +32,13 @@ test.describe('Filters', () => {
     expect(page.url().includes('?filters')).toBeFalsy()
     await expect(page.locator('[data-filter="tag"]').first()).toBeHidden()
   })
+
   test('Should open Filter popup', async ({ page }) => {
     const detailedFilterButton = page.locator('[data-filter="detailedButton"]').locator('visible=true').first()
     await detailedFilterButton.click()
     await expect(page.locator('[data-filter ="popup"]', { hasText: 'Фильтры' })).toBeVisible()
   })
+
   test('Should change tags', { tag: '@only-desktop' }, async ({ page }) => {
     const detailedFilterButton = page.locator('[data-filter="detailedButton"]').locator('visible=true').first()
     await detailedFilterButton.click()
@@ -54,62 +59,4 @@ test.describe('Filters', () => {
     await expect(tagWithTwoOptions).toHaveText(await tagWithOneOption.innerText())
     expect(tagWithOneOption !== tagWithTwoOptions).toBeTruthy()
   })
-})
-
-test('', async ({ page }) => {
-  const testObject = {
-    id: 30,
-    name: 'Аппартаменты 1',
-    complexName: null,
-    address: null,
-    seoUrl: 'appartamenty-1',
-    minArea: 85,
-    typeObject: 'secondary-housing',
-    description: 'Описание аппартаментов 1',
-    isReserved: false,
-    price: 123456789,
-    priceDiscount: 1450000,
-    userId: null,
-    gallery: {
-      images: ['/images/penthouses.png', '/images/small-house.png', '/images/dormitory.png', '/images/penthouses.png'],
-      count: 1,
-    },
-    completionDate: '2024-09-17 08:05:47',
-    publicationTime: '2024-09-16 20:28:27',
-    tags: [
-      {
-        id: 1,
-        name: 'tag test',
-      },
-      {
-        id: 2,
-        name: 'tag 2 test',
-      },
-    ],
-    characteristics: [
-      {
-        id: 65,
-        value: '2 комн',
-      },
-      {
-        id: 1080,
-        value: ' м²',
-      },
-      {
-        id: 1115,
-        value: ' этаж',
-      },
-    ],
-  }
-
-  await page.route(`${process.env.NEXT_PUBLIC_API_URL}/catalog/secondary-housing`, async (route) => {
-    const response = await route.fetch()
-    const json = await response.json()
-    json.push(testObject)
-    await route.fulfill({ response, json })
-  })
-
-  await page.goto(ROUTES.SECONDARY_HOUSING)
-
-  await expect(page.getByText(`${testObject.price}`, { exact: true })).toBeVisible({ timeout: 10000 })
 })
