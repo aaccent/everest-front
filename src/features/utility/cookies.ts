4a0befc909e183401e338136102e70da7c6ee3a3
sevastyan <seva@aaccent.ru>
2024-10-07 12:03:47 +0300

    docs: add comments and JSDoc to features/cookies.ts

--- src/features/utility/cookies.ts
+++ src/features/utility/cookies.ts
@@ -4,25 +4,28 @@ export const COOKIES = {
 
 export type CookieName = (typeof COOKIES)[keyof typeof COOKIES]
 
+// 30 дней * 24 часов * 60 минут * 60 секунд
 export const ONE_MONTH = 30 * 24 * 60 * 60
 export const ONE_YEAR = ONE_MONTH * 12
 
+/** Парсит строку `cookiesStr` вида `name=value;name2=value;` в объект для дальнейшей работы */
 export function parseCookies(cookiesStr: string) {
   const cookies = cookiesStr
+    // Делим на записи между ";"
     .split(';')
+    // Делим на ключ=значение
     .map((i) => i.trim().split('='))
+    // Превращаем в объект
     .map((i) => ({ name: i[0], value: i[1] }))
 
   return {
     get(name: CookieName | string) {
       return cookies.find((i) => i.name === name)
     },
-    all() {
-      return cookies
-    },
   }
 }
 
+/** Достаёт строку куков из `document.cookie` и парсит с помощью [parseCookies()]{@link parseCookies} */
 export function cookies() {
   if (typeof window === 'undefined') return
 
@@ -30,7 +33,9 @@ export function cookies() {
 }
 
 interface CookieOptions {
+  /** @default [ONE_MONTH]{@link ONE_MONTH} */
   maxAge?: number
+  /** @default / */
   path?: string
 }
 
