7ca502963677b1ecb18bb412dd75fb852fffb6b5
Regina <rmyachina@mail.ru>
2024-10-07 17:50:12 +0300

    feat: city popup

--- src/components/GeoPositionNotification.tsx
+++ src/components/GeoPositionNotification.tsx
@@ -1,9 +1,10 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { CityContext } from '@/globals/CityContext'
 import Button from '@/ui/buttons/Button'
-import { hideScroll, showScroll } from '@/features/scroll'
+import { showScroll } from '@/features/scroll'
 import { City } from '@/types/Geo'
+import { PopupContext } from '@/features/Popup'
 
 interface Props {
   cityByIp: City
@@ -12,20 +13,27 @@ interface Props {
 function GeoPositionNotification({ cityByIp }: Props) {
   const [shown, setShown] = useState<boolean>(true)
   const { setCurrentCity } = useContext(CityContext)
+  const { openPopup } = useContext(PopupContext)
 
-  useEffect(() => {
-    if (shown) hideScroll()
-  }, [])
-
-  const onOkCityClick = () => {
+  const onOkCityButtonClick = () => {
     setCurrentCity(cityByIp)
     setShown(false)
     showScroll()
   }
 
+  const onChangeGeoButtonClick = () => {
+    setShown(false)
+    openPopup({
+      name: 'geoPopup',
+      args: {
+        selectGeoAuto: onOkCityButtonClick,
+      },
+    })
+  }
+
   return (
     shown && (
-      <div className='fixed inset-0 z-50 bg-base-650'>
+      <div className='fixed inset-0 z-50 bg-base-650 md:bg-transparent'>
         <div className='absolute inset-x-0 bottom-0 rounded-t-[24px] bg-base-100 px-[24px] pb-[32px] pt-[24px] md:inset-x-auto md:right-[169px] md:top-[44px] md:h-fit md:w-[320px] md:rounded-b-[24px] md:pb-[24px]'>
           <div className='text-header-300 md:text-base-200-lg-100 mb-[32px] md:mb-[18px]'>
             <span className='text-base-650'>Ваш город — </span>
@@ -37,11 +45,17 @@ function GeoPositionNotification({ cityByIp }: Props) {
               type='button'
               variation='primary'
               size='medium'
-              onClick={onOkCityClick}
+              onClick={onOkCityButtonClick}
             >
               Верно
             </Button>
-            <Button className='order-1 md:order-2' type='button' variation='second' size='medium'>
+            <Button
+              className='order-1 md:order-2'
+              type='button'
+              variation='second'
+              size='medium'
+              onClick={onChangeGeoButtonClick}
+            >
               сменить
             </Button>
           </div>
