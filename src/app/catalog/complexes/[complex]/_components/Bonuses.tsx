c22816372bb8d31783e28b978ecf4982a04ff258
Regina <rmyachina@mail.ru>
2024-10-09 13:20:19 +0300

    feat: bonuses block

--- src/app/catalog/complexes/[complex]/_components/Bonuses.tsx
+++ src/app/catalog/complexes/[complex]/_components/Bonuses.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Section from '@/layout/Section'
-import Carousel, { CarouselInner, CarouselSlide } from '@/components/Carousel/Carousel'
+import Carousel, {
+  CarouselInner,
+  CarouselNavigations,
+  CarouselProgressBar,
+  CarouselSlide,
+} from '@/components/Carousel/Carousel'
 import Img from '@/ui/Img'
 
 interface Bonus {
@@ -15,8 +20,25 @@ const testBonuses = [
     id: 1,
     image: '/bonuses/image 63.png',
     title: 'Hoff',
-    description:
-      'Скидка 20% на мебельСкидка 20% на мебельСкидка 20% на мебельСкидка 20% на мебельСкидка 20% на мебельСкидка 20% на мебель',
+    description: 'Скидка 20% на мебель',
+  },
+  {
+    id: 2,
+    image: '/bonuses/image 631.png',
+    title: 'Многомебели',
+    description: 'Скидка 15% на мебель',
+  },
+  {
+    id: 3,
+    image: '/bonuses/image 632.png',
+    title: 'Leroy Merlin',
+    description: 'Скидка 20 000 рублей',
+  },
+  {
+    id: 1,
+    image: '/bonuses/image 63.png',
+    title: 'Hoff',
+    description: 'Скидка 20% на мебель',
   },
   {
     id: 2,
@@ -38,17 +60,19 @@ function Bonuses() {
       return (
         <CarouselSlide
           key={b.id}
-          className='min-h-[260px] w-[320px] rounded-b-[20px] rounded-tl-[20px] border border-base-400 px-[20px] pb-[20px] pt-[72px]'
+          className='min-h-[260px] w-full max-w-[320px] pr-[12px] md:max-w-none md:basis-1/3 md:pr-[16px]'
         >
-          <Img
-            src={b.image}
-            width={244}
-            height={137}
-            className='ml-auto mr-auto h-[85px] w-[152px] object-cover object-center'
-          />
-          <div className='mt-auto'>
-            <div>{b.title}</div>
-            <div>{b.description}</div>
+          <div className='flex size-full flex-col rounded-[20px] border border-base-400 px-[20px] pb-[20px] pt-[72px] md:max-w-[512px] md:px-[32px] md:pb-[32px] md:pt-[98px]'>
+            <Img
+              src={b.image}
+              width={244}
+              height={137}
+              className='ml-auto mr-auto h-[85px] w-[152px] object-cover object-center md:mb-[42px] md:h-[137px] md:w-[244px]'
+            />
+            <div className='mt-auto'>
+              <div className='text-header-300 mb-[4px] md:mb-[8px]'>{b.title}</div>
+              <div className='text-base-200-lg-100 text-base-650'>{b.description}</div>
+            </div>
           </div>
         </CarouselSlide>
       )
@@ -57,9 +81,11 @@ function Bonuses() {
 
   return (
     <Section>
-      <Carousel>
+      <Carousel className='relative'>
         <div className='text-header-200 mb-[40px] uppercase'>Бонусы от партнеров</div>
+        <CarouselNavigations />
         <CarouselInner>{showBonuses()}</CarouselInner>
+        <CarouselProgressBar className='mt-[64px] hidden md:block' />
       </Carousel>
     </Section>
   )
