9a2c22c982c03dedbdf7b99b3c6a73857853124c
Regina <rmyachina@mail.ru>
2024-10-14 17:23:45 +0300

    fix: pr issues

--- src/app/catalog/complexes/[complex]/_components/ExclusiveOffers.tsx
+++ src/app/catalog/complexes/[complex]/_components/ExclusiveOffers.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getExclusiveOffers } from '@/globals/api/methods/catalog-details/getExclusiveOffers'
+import { getExclusiveOffers } from '@/globals/api'
 import { DecorativeBlock } from '@/layout/DecorativeSection'
 import Img from '@/ui/Img'
 import bavel from '@/assets/static/decorative-bg/exclusiveOffersBavel.svg'
@@ -21,17 +21,17 @@ async function ExclusiveOffers() {
   const offersBlock = await getExclusiveOffers()
 
   function showOffers() {
-    return offersBlock.items.map((o) => {
+    return offersBlock.items.map((offer) => {
       return (
-        <div key={o.id} className='relative w-full rounded-[24px] bg-base-100/10 p-[20px] md:p-[24px]'>
+        <li key={offer.id} className='relative w-full rounded-[24px] bg-base-100/10 p-[20px] md:p-[24px]'>
           <div className='mb-[12px] flex w-full justify-between md:mb-[33px]'>
-            <div className='text-base-100-reg-100 max-w-[200px] text-base-100'>{o.title}</div>
+            <div className='text-base-100-reg-100 max-w-[200px] text-base-100'>{offer.title}</div>
             <Img src={logo} className='w-full max-w-[32px] opacity-20 md:max-w-[49px]' />
           </div>
-          <div className='text-base-100/50'>{o.text}</div>
+          <div className='text-base-100/50'>{offer.text}</div>
           <Img src={bavel} className='absolute bottom-0 right-0 hidden filter-primary md:block' />
           <Img src={mob_bavel} className='absolute bottom-0 right-0 filter-primary md:hidden' />
-        </div>
+        </li>
       )
     })
   }
@@ -40,7 +40,7 @@ async function ExclusiveOffers() {
       <h2 className='text-header-100 mb-[24px] uppercase text-base-100 md:mb-[48px] md:w-full md:max-w-[550px]'>
         {offersBlock.title}
       </h2>
-      <div className='flex flex-col gap-[12px] md:grid md:grid-cols-3 md:gap-[18px]'>{showOffers()}</div>
+      <ul className='flex flex-col gap-[12px] md:grid md:grid-cols-3 md:gap-[18px]'>{showOffers()}</ul>
     </DecorativeBlock>
   )
 }
