931797feedc9e630bc5b1a056dae923f1cc6c629
Regina <rmyachina@mail.ru>
2024-10-14 17:08:48 +0300

    fix: pr issues

--- src/app/catalog/complexes/[complex]/_components/Bonuses.tsx
+++ src/app/catalog/complexes/[complex]/_components/Bonuses.tsx
@@ -7,7 +7,7 @@ import Carousel, {
   CarouselSlide,
 } from '@/components/Carousel/Carousel'
 import Img from '@/ui/Img'
-import { getBonuses } from '@/globals/api/methods/catalog-details/getBonuses'
+import { getBonuses } from '@/globals/api'
 
 export interface Bonus {
   id: number
@@ -18,23 +18,24 @@ export interface Bonus {
 
 async function Bonuses() {
   const bonuses = await getBonuses()
+
   function showBonuses() {
-    return bonuses.map((b) => {
+    return bonuses.map((bonus) => {
       return (
         <CarouselSlide
-          key={b.id}
+          key={bonus.id}
           className='min-h-[260px] w-full max-w-[320px] pr-[12px] md:max-w-none md:basis-1/3 md:pr-[16px]'
         >
           <div className='flex size-full flex-col rounded-[20px] border border-base-400 px-[20px] pb-[20px] pt-[72px] md:max-w-[512px] md:px-[32px] md:pb-[32px] md:pt-[98px]'>
             <Img
-              src={b.photoPath}
+              src={bonus.photoPath}
               width={244}
               height={137}
               className='ml-auto mr-auto h-[85px] w-[152px] object-cover object-center md:mb-[42px] md:h-[137px] md:w-[244px]'
             />
             <div className='mt-auto'>
-              <div className='text-header-300 mb-[4px] md:mb-[8px]'>{b.title}</div>
-              <div className='text-base-200-lg-100 text-base-650'>{b.text}</div>
+              <div className='text-header-300 mb-[4px] md:mb-[8px]'>{bonus.title}</div>
+              <div className='text-base-200-lg-100 text-base-650'>{bonus.text}</div>
             </div>
           </div>
         </CarouselSlide>
@@ -45,7 +46,7 @@ async function Bonuses() {
   return (
     <Section>
       <Carousel className='relative'>
-        <div className='text-header-200 mb-[40px] uppercase'>Бонусы от партнеров</div>
+        <h2 className='text-header-200 mb-[40px] uppercase'>Бонусы от партнеров</h2>
         <CarouselNavigations />
         <CarouselInner>{showBonuses()}</CarouselInner>
         <CarouselProgressBar className='mt-[64px] hidden md:block' />
