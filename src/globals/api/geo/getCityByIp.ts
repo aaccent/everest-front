94f80a9c048414bf5d4e5b2107828d18853e8b56
foxhable <foxhable@gmail.com>
2024-10-15 09:57:43 +0300

    refactor: move DADATA_URL from env

--- src/globals/api/geo/getCityByIp.ts
+++ src/globals/api/geo/getCityByIp.ts
@@ -1,6 +1,8 @@
 import { headers } from 'next/headers'
 import { EXAMPLE_IP } from '@/middleware'
 
+const DADATA_URL = 'http://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?language=ru&ip='
+
 const options = {
   method: 'GET',
   headers: {
@@ -13,11 +15,12 @@ const options = {
 export async function getCityByIp(): Promise<string> {
   const ip = (headers().get('x-forwarded-for') ?? EXAMPLE_IP).split(',')[0]
 
-  const url = process.env.DADATA_URL + ip
+  const url = DADATA_URL + ip
   return await fetch(url, options)
     .then((response) => response.json())
     .then((result) => result.location.value)
     .catch((error) => {
+      console.error(error)
       return 'unknown'
     })
 }
