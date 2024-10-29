1963450b94c2daa81768baaef61b070b89ec5fea
Regina <rmyachina@mail.ru>
2024-10-29 15:53:33 +0300

    feat: contacts page

---
+++ src/app/contacts/_components/OfficesMap.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+import CustomMap from '@/components/CustomMap'
+
+function OfficesMap() {
+  return <CustomMap></CustomMap>
+}
+
+export default OfficesMap
---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import OfficesMap from '@/app/contacts/_components/OfficesMap'
+
+function Page() {
+  return <OfficesMap />
+}
+
+export default Page
