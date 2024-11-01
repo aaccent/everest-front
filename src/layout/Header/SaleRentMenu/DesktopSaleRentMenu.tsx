a0437266084c9b6935abeaf2d684cebad8f0f363
sevastyan <seva@aaccent.ru>
2024-11-01 13:22:29 +0300

    fix: styles and fields output of sale and rent menu

--- src/layout/Header/SaleRentMenu/DesktopSaleRentMenu.tsx
+++ src/layout/Header/SaleRentMenu/DesktopSaleRentMenu.tsx
@@ -3,7 +3,7 @@ import { IsDesktop } from '@/features/adaptive'
 import Link from 'next/link'
 import Image from 'next/image'
 import { suggestionPlural } from '@/features/utility/pluralRules'
-import { getCatalogMenu, MenuType } from '@/globals/api'
+import { getSaleRentMenu, MenuType } from '@/globals/api'
 import { generateCategoryLink } from '@/features/link'
 
 interface Props {
@@ -12,50 +12,50 @@ interface Props {
 }
 
 async function DesktopSaleRentMenu({ className, category }: Props) {
-  const data = await getCatalogMenu(category)
-  const fullAmount = data.reduce((inc, item) => inc + item.total, 0)
+  const data = await getSaleRentMenu(category)
+  const fullAmount = data.reduce((sum, item) => sum + item.count, 0)
   const title = category === 'sale' ? 'Покупка' : 'Аренда'
 
   function showCategories() {
-    return data.map((item) => {
-      return (
-        <li key={item.id}>
-          <Link
-            className='flex items-center gap-[12px] rounded-[24px] bg-base-200 p-[16px] md:gap-[20px] md:bg-transparent md:p-[10px] md:transition-colors md:hover:bg-base-100'
-            href={generateCategoryLink(item)}
-          >
-            {item.iconUrl ? (
-              <Image
-                src={item.iconUrl}
-                alt=''
-                width={86}
-                height={86}
-                className='size-[86px] object-cover object-center'
-              />
-            ) : (
-              <div className='size-[86px] rounded-[24px] bg-primary'></div>
-            )}
-            <div className='w-[140px]'>
-              <div className='text-base-300-reg-100-upper mb-[6px] text-base-600'>{item.name}</div>
-              <div className='text-base-400-lg-100 text-base-650'>{`${item.total} ${suggestionPlural.get(item.total)}`}</div>
-            </div>
-          </Link>
-        </li>
-      )
-    })
+    return data.map((item) => (
+      <li key={item.id}>
+        <Link
+          className='flex items-center gap-[12px] rounded-[24px] bg-base-200 p-[16px] md:gap-[20px] md:bg-transparent md:p-[10px] md:transition-colors md:hover:bg-base-100'
+          href={generateCategoryLink(item)}
+        >
+          {item.imageUrl ? (
+            <Image
+              src={item.imageUrl}
+              alt=''
+              width={86}
+              height={86}
+              className='size-[86px] object-cover object-center'
+            />
+          ) : (
+            <div className='size-[86px] rounded-[24px] bg-primary'></div>
+          )}
+          <div className='w-[140px]'>
+            <div className='text-base-300-reg-100-upper mb-[6px] text-base-600'>{item.name}</div>
+            <div className='text-base-400-lg-100 text-base-650'>{`${item.count} ${suggestionPlural.get(item.count)}`}</div>
+          </div>
+        </Link>
+      </li>
+    ))
   }
 
   return (
     <>
       <IsDesktop>
         <div
-          className={`px-container fixed inset-x-0 top-[115px] z-20 hidden w-full gap-[16px] bg-base-100 pb-[56px] pt-[32px] ${className}`}
+          className={`px-container fixed inset-x-0 top-[115px] z-20 hidden h-[580px] w-full gap-[16px] rounded-b-[32px] bg-base-100 pb-[56px] pt-[32px] ${className}`}
           data-menu-type={category}
         >
-          <div className='w-[908px] rounded-[32px] bg-base-300 p-[40px]'>
-            <span className='text-header-300 text-base-600'>{`${title} — `}</span>
-            <span className='text-header-300 text-base-650'>{`${fullAmount} ${suggestionPlural.get(fullAmount)}`}</span>
-            <ul className='ml-[-10px] mt-[40px] grid auto-rows-max grid-cols-3 gap-x-[12px] gap-y-[13px] transition-opacity'>
+          <div className='flex w-full max-w-[900px] flex-col rounded-[32px] bg-base-300 p-[40px]'>
+            <div>
+              <span className='text-header-300 text-base-600'>{`${title} — `}</span>
+              <span className='text-header-300 text-base-650'>{`${fullAmount} ${suggestionPlural.get(fullAmount)}`}</span>
+            </div>
+            <ul className='ml-[-10px] mt-[40px] grid h-1 grow auto-rows-max grid-cols-3 gap-x-[12px] gap-y-[13px] overflow-y-auto transition-opacity scrollbar-custom'>
               <li>
                 <Link
                   className='flex items-center gap-[12px] rounded-[24px] bg-base-200 p-[16px] md:gap-[20px] md:bg-transparent md:p-[10px] md:transition-colors md:hover:bg-base-100'
@@ -68,7 +68,6 @@ async function DesktopSaleRentMenu({ className, category }: Props) {
                   </div>
                 </Link>
               </li>
-
               {showCategories()}
             </ul>
           </div>
