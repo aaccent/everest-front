2194b6cd372a4f51ffaefc3a7fc7b3ba6f901e45
sevastyan <seva@aaccent.ru>
2024-11-01 12:39:53 +0300

    fix: overlaying GeoPosition notification

--- src/app/(main-page)/page.tsx
+++ src/app/(main-page)/page.tsx
@@ -7,7 +7,6 @@ import UsefulLinks from './_components/UsefulLinks/UsefulLinks'
 
 import { getNewBuildingsOnMain, getNewObjects } from '@/globals/api'
 import { ROUTES } from '@/globals/paths'
-import GeoPosition from '@/components/GeoPosition'
 
 export default async function Home() {
   const newBuildingsOnMain = await getNewBuildingsOnMain().catch(() => [])
@@ -16,7 +15,6 @@ export default async function Home() {
   return (
     <>
       <MainHero />
-      <GeoPosition />
       <SliderSection type='complex' link={ROUTES.COMPLEXES} list={newBuildingsOnMain} title='Новостройки' />
       <Services />
       <SliderSection type='objects' link={ROUTES.SECONDARY_HOUSING} list={newObjects} title='Новинки в каталоге' />
