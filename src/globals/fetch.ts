178a2ffaa9a0b22dc80baaae9dc6160d67c38a64
foxhable <foxhable@gmail.com>
2024-11-02 09:58:00 +0300

    refactor: move 'tryJSONParse()' to globals/fetch

--- src/globals/fetch.ts
+++ src/globals/fetch.ts
@@ -10,3 +10,17 @@ export function syncTryJSONParse<TType extends any = any>(value: any): TType | f
     return false
   }
 }
+
+/**
+ * Преобразовывает строку в JSON Объект.
+ * Оборачивает try catch объявление в `async` функцию для использования `await` оператора
+ * @typeParam TType - тип возвращаемый при успешной конвертации
+ * @return JSON Объект если успешно, иначе `false`
+ * */
+export async function tryJSONParse<TType extends any = any>(value: any): Promise<TType | false> {
+  try {
+    return JSON.parse(value)
+  } catch {
+    return false
+  }
+}
