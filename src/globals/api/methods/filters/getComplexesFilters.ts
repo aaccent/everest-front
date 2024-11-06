a8832b02ab586dc6c3f6257352e9688aeb5566d7
sevastyan <seva@aaccent.ru>
2024-11-06 15:43:31 +0300

    fix: error on complexes list page because format of getComplexFilters

--- src/globals/api/methods/filters/getComplexesFilters.ts
+++ src/globals/api/methods/filters/getComplexesFilters.ts
@@ -1,17 +1,22 @@
-import { apiCall, APIResponse } from '@/globals/api/apiCall'
 import { Filters } from '@/types/FiltersType'
 
 export async function getComplexesFilters(): Promise<Filters> {
-  try {
-    const res = await apiCall<false, APIResponse<Filters>>('/filter/complex/detail', {
-      method: 'GET',
-    })
+  // Temporal fix while backend doesn't fix output format
+  // try {
+  //   const res = await apiCall<false, APIResponse<Filters>>('/filter/complex/detail', {
+  //     method: 'GET',
+  //   })
+  //
+  //   return res.data
+  // } catch {
+  //   return {
+  //     filters: [],
+  //     sorts: [],
+  //   }
+  // }
 
-    return res.data
-  } catch {
-    return {
-      filters: [],
-      sorts: [],
-    }
+  return {
+    filters: [],
+    sorts: [],
   }
 }
