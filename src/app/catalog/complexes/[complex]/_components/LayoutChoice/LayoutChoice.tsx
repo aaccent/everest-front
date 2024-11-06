98d671a65c07074894a7d59bddc6a30a07040edc
sevastyan <seva@aaccent.ru>
2024-11-06 14:59:53 +0300

    refactor: remove quick filters from detail complex page

--- src/app/catalog/complexes/[complex]/_components/LayoutChoice/LayoutChoice.tsx
+++ src/app/catalog/complexes/[complex]/_components/LayoutChoice/LayoutChoice.tsx
@@ -7,8 +7,6 @@ import LayoutInner from '@/app/catalog/complexes/[complex]/_components/LayoutCho
 import LayoutTypes from '@/app/catalog/complexes/[complex]/_components/LayoutChoice/LayoutTypes'
 import { LayoutContextProvider } from './LayoutListContext'
 import ObjectsTable from './ObjectsTable'
-import { FilterItems } from '@/components/FilterItems'
-import { getComplexQuickFilters } from '@/globals/api'
 import ResetFiltersButton from '@/components/QuickFilter/ResetFiltersButton'
 
 export const LAYOUT_ID = 'complex-layout'
@@ -18,8 +16,6 @@ interface LayoutChoiceProps {
 }
 
 async function LayoutChoice({ complex }: LayoutChoiceProps) {
-  const quickFilters = await getComplexQuickFilters()
-
   return (
     <Section>
       <h2 className='text-header-200 mb-[32px] font-coolvetica uppercase' id={LAYOUT_ID}>
@@ -27,9 +23,7 @@ async function LayoutChoice({ complex }: LayoutChoiceProps) {
       </h2>
       <Container className='mb-[40px] hidden justify-between gap-[20px] rounded-[32px] bg-base-200 md:flex md:px-[32px] md:py-[32px]'>
         <div className='flex items-center gap-[24px]'>
-          <div className='flex items-center gap-[16px]'>
-            <FilterItems filters={quickFilters.filters} isQuick />
-          </div>
+          <div className='flex items-center gap-[16px]'></div>
           <ResetFiltersButton text='сбросить все' />
         </div>
         <Button className='ml-auto' variation='primary' size='small' text='Показать 442 объекта' />
