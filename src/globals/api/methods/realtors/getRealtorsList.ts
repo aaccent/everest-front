372bf65e63991e54e5058d75f4c115b08d9088fe
regina <rmyachina@mail.ru>
2024-11-15 12:07:12 +0300

    fix: type error

--- src/globals/api/methods/realtors/getRealtorsList.ts
+++ src/globals/api/methods/realtors/getRealtorsList.ts
@@ -1,5 +1,5 @@
 import realtors from '@/globals/mock-data/realtors-list.json'
-import { RealtorCardType } from '@/app/realtors/_components/RealtorsList'
+import { RealtorCardType } from '@/types/Realtor'
 
 export async function getRealtorsList(subString: string | null = null): Promise<RealtorCardType[]> {
   return realtors
