d0e6ed29a00a01c46224052bbbf144d47737adad
foxhable <foxhable@gmail.com>
2024-11-19 10:38:31 +0300

    feat: output if no genplan prop on detail complex page

--- src/app/catalog/complexes/[complex]/_components/LayoutChoice/LayoutTypes.tsx
+++ src/app/catalog/complexes/[complex]/_components/LayoutChoice/LayoutTypes.tsx
@@ -20,18 +20,20 @@ async function LayoutTypes({ complex }: Props) {
   return (
     <div className='flex w-full flex-col-reverse gap-[32px] md:flex-row md:gap-[50px]'>
       <div className='grid w-full grid-cols-1 gap-[8px] md:grid-cols-3 md:gap-[16px]'>{showList()}</div>
-      <div className='flex flex-shrink-0 flex-col-reverse gap-[16px] md:w-[280px] md:flex-col'>
-        <div className='flex flex-col gap-[4px]'>
-          <span className='text-base-100-lg-100'>Генплан проекта</span>
-          <div className='text-base-300-lg-100 flex gap-[20px] text-base-600/50 md:hidden'>
-            <span>{complex.address}</span>
-            <span>
-              {complex.objectCount} {flatPlural.get(complex.objectCount)}
-            </span>
+      {!!complex.genPlanImg && (
+        <div className='flex flex-shrink-0 flex-col-reverse gap-[16px] md:w-[280px] md:flex-col'>
+          <div className='flex flex-col gap-[4px]'>
+            <span className='text-base-100-lg-100'>Генплан проекта</span>
+            <div className='text-base-300-lg-100 flex gap-[20px] text-base-600/50 md:hidden'>
+              <span>{complex.address}</span>
+              <span>
+                {complex.objectCount} {flatPlural.get(complex.objectCount)}
+              </span>
+            </div>
           </div>
+          <GenPlanButton complex={complex} />
         </div>
-        <GenPlanButton complex={complex} />
-      </div>
+      )}
     </div>
   )
 }
