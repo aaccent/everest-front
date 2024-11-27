6cbe2b8de69eeb119b9297509fb9a057d94bc901
regina <rmyachina@mail.ru>
2024-11-27 12:54:04 +0300

    feat: format publication time in card

--- src/ui/cards/ObjectCard/ObjectListCard.tsx
+++ src/ui/cards/ObjectCard/ObjectListCard.tsx
@@ -6,6 +6,7 @@ import { generateObjectLink } from '@/features/link'
 import Gallery from '@/ui/cards/ObjectCard/Gallery'
 import Tags from '@/components/Tags'
 import { TEST_ID } from '@/globals/testIds'
+import { formatTime } from '@/features/utility/time'
 
 interface Props {
   item: DefaultObject
@@ -39,7 +40,7 @@ function ObjectListCard({ item }: Props) {
           <ActionButton className='!bg-base-300 before:bg-icon-heart before:!filter-base-600' />
         </div>
         <span className='mt-auto flex gap-[4px] text-base-600/50 before:relative before:top-[1px] before:block before:size-[19px] before:bg-icon-clip before:opacity-50 before:filter-base-600 before:bg-default'>
-          5 мин назад
+          {formatTime(item.publicationTime)}
         </span>
       </div>
     </div>
