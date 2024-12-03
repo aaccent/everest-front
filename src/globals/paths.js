818d332c3861d7459565218626dfd1b6e1b3219e
foxhable <foxhable@gmail.com>
2024-12-03 12:25:19 +0300

    fix: route to new-building flats

--- src/globals/paths.js
+++ src/globals/paths.js
@@ -3,7 +3,7 @@ export const PATHS = {
   NEW_BUILDINGS: 'new-building',
   COMPLEXES: 'complexes',
   SECONDARY_HOUSING: 'secondary-housing',
-  APARTMENTS: 'apartments',
+  APARTMENTS: 'flat',
   MAP: 'map',
   COMPLEX_OBJECTS: 'complex-objects',
   CONTACTS: 'contacts',
