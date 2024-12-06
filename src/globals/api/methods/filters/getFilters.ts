b9c1332e90531b5ec811ca4b45e5e93cb658b969
sevastyan <seva@aaccent.ru>
2024-12-06 11:38:30 +0300

    feat: errors handling in getFilters api methods

--- src/globals/api/methods/filters/getFilters.ts
+++ src/globals/api/methods/filters/getFilters.ts
@@ -1,15 +1,20 @@
 import { apiCall, APIResponse, SlashPath } from '@/globals/api/apiCall'
 import { Filters } from '@/types/FiltersType'
+import { EMPTY_FILTERS } from '@/globals/filters'
 
 type Request = {
   complexSeo?: string
 }
 
 export async function getFilters(objectType: string, complexSeo?: string) {
-  const path: SlashPath = `/filter/${objectType}`
-  const res = await apiCall<false | Request, APIResponse<Filters>>(path, {
-    method: 'GET',
-    request: complexSeo ? { complexSeo } : undefined,
-  })
-  return res.data
+  try {
+    const path: SlashPath = `/filter/${objectType}`
+    const res = await apiCall<false | Request, APIResponse<Filters>>(path, {
+      method: 'GET',
+      request: complexSeo ? { complexSeo } : undefined,
+    })
+    return res.data
+  } catch {
+    return EMPTY_FILTERS
+  }
 }
--- src/globals/api/methods/filters/getQuickFilters.ts
+++ src/globals/api/methods/filters/getQuickFilters.ts
@@ -1,15 +1,20 @@
 import { apiCall, APIResponse, SlashPath } from '@/globals/api/apiCall'
 import { QuickFilters } from '@/types/FiltersType'
+import { EMPTY_FILTERS } from '@/globals/filters'
 
 type Request = {
   complexSeo?: string
 }
 
 export async function getQuickFilters(objectType: string, complexSeo?: string) {
-  const path: SlashPath = `/filter/${objectType}/main`
-  const res = await apiCall<Request | false, APIResponse<QuickFilters>>(path, {
-    method: 'GET',
-    request: complexSeo ? { complexSeo } : undefined,
-  })
-  return res.data
+  try {
+    const path: SlashPath = `/filter/${objectType}/main`
+    const res = await apiCall<Request | false, APIResponse<QuickFilters>>(path, {
+      method: 'GET',
+      request: complexSeo ? { complexSeo } : undefined,
+    })
+    return res.data
+  } catch {
+    return EMPTY_FILTERS
+  }
 }
